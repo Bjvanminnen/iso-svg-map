d96bb944063a72b0af3903f5594dc4c1e09987c4
Brent Van Minnen <bjvanminnen@gmail.com>
2017-04-22 13:04:21 -0700

    drawing iso grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import {
+  isoCell
+} from './pathGen';
 
 class App extends Component {
   render() {
+    const coordinates = [];
+    for (let x = 0; x < 5; x++) {
+      for (let y = 0; y < 5; y++) {
+        coordinates.push([x, y]);
+      }
+    }
+
     return (
-      <svg height="210" width="400">
-        <path d="M150 0 L75 200 L225 200 Z" />
+      <svg height="500" width="800">
+        <g transform="translate(400, 50)">
+          {coordinates.map(coords => (
+            <path
+              d={isoCell(...coords)}
+              fill="none"
+              stroke="black"
+            />
+          ))}
+        </g>
       </svg>
     );
   }
--- src/pathGen.js
+++ src/pathGen.js
@@ -1,5 +1,23 @@
-export const path = (...args) => args.join(' ');
+export const pathFrom = (...args) => args.join(' ');
 
-export const moveTo = (x, y) => `M ${x} ${y}`;
-export const lineTo = (x, y) => `L ${x} ${y}`;
+export const moveTo = (x, y) => `M${x} ${y}`;
+export const lineTo = (x, y) => `L${x} ${y}`;
 export const closePath = () => 'Z';
+
+const TILE_WIDTH_HALF = 64;
+const TILE_HEIGHT_HALF = 32;
+
+export const isoPoint = (mapX, mapY) => ([
+  (mapX - mapY) * TILE_WIDTH_HALF,
+  (mapX + mapY) * TILE_HEIGHT_HALF,
+]);
+
+export const isoCell = (mapX, mapY) => (
+  pathFrom(
+    moveTo(...isoPoint(mapX, mapY)),
+    lineTo(...isoPoint(mapX + 1, mapY)),
+    lineTo(...isoPoint(mapX + 1, mapY + 1)),
+    lineTo(...isoPoint(mapX, mapY + 1)),
+    closePath()
+  )
+);
