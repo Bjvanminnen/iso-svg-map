1df765e7dddba7c7063c1fd795ebc9e5bdbb0d62
Brent Van Minnen <bjvanminnen@gmail.com>
2017-04-23 10:35:54 -0700

    height working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
 import {
+  isoPoint,
   isoCell
 } from './pathGen';
 
 class App extends Component {
   render() {
-    const coordinates = [];
+    const heights = {
+      '1_2': 1,
+      '1_3': 1,
+      '1_4': 1,
+      '2_1': 1,
+      '2_2': 1,
+      '2_3': 1,
+      '2_4': 1,
+      '3_2': 1,
+      '3_3': 1,
+    };
+
+    const points = [];
+    for (let x = 0; x < 6; x++) {
+      points[x] = [];
+      for (let y = 0; y < 6; y++) {
+        points[x][y] = isoPoint(x, y, heights[`${x}_${y}`] || 0);
+      }
+    }
+
+    // points[5][5] = isoPoint(5, 5, 0);
+
+
+    const cells = [];
     for (let x = 0; x < 5; x++) {
       for (let y = 0; y < 5; y++) {
-        coordinates.push([x, y]);
+        cells.push({x, y});
       }
     }
 
     return (
       <svg height="500" width="800">
         <g transform="translate(400, 50)">
-          {coordinates.map(coords => (
+          {cells.map(cell => (
             <path
-              d={isoCell(...coords)}
-              fill="none"
+              key={[cell.x,cell.y].join('.')}
+              d={isoCell(points, cell.x, cell.y)}
+              fill="#008000"
               stroke="black"
+              fillOpacity={0.8}
             />
           ))}
         </g>
--- src/pathGen.js
+++ src/pathGen.js
@@ -7,17 +7,31 @@ export const closePath = () => 'Z';
 const TILE_WIDTH_HALF = 64;
 const TILE_HEIGHT_HALF = 32;
 
-export const isoPoint = (mapX, mapY) => ([
-  (mapX - mapY) * TILE_WIDTH_HALF,
-  (mapX + mapY) * TILE_HEIGHT_HALF,
-]);
+export const isoPoint = (mapX, mapY, height) => {
+  return ([
+    (mapX - mapY) * TILE_WIDTH_HALF,
+    (mapX + mapY) * TILE_HEIGHT_HALF - 20 * height,
+  ]);
+}
 
-export const isoCell = (mapX, mapY) => (
-  pathFrom(
-    moveTo(...isoPoint(mapX, mapY)),
-    lineTo(...isoPoint(mapX + 1, mapY)),
-    lineTo(...isoPoint(mapX + 1, mapY + 1)),
-    lineTo(...isoPoint(mapX, mapY + 1)),
+export const isoCell = (points, mapX, mapY) => {
+  const top = points[mapX][mapY];
+  const right = points[mapX + 1][mapY];
+  const bottom = points[mapX + 1][mapY + 1];
+  const left = points[mapX][mapY + 1];
+
+  return pathFrom(
+    moveTo(...top),
+    lineTo(...right),
+    lineTo(...bottom),
+    lineTo(...left),
     closePath()
-  )
-);
+  );
+  // pathFrom(
+  //   moveTo(...isoPoint(mapX, mapY)),
+  //   lineTo(...isoPoint(mapX + 1, mapY)),
+  //   lineTo(...isoPoint(mapX + 1, mapY + 1)),
+  //   lineTo(...isoPoint(mapX, mapY + 1)),
+  //   closePath()
+  // )
+}
