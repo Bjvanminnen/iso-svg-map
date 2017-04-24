23e7ed77ec7ecb98735d0be0686e92f3b6a1b901
Brent Van Minnen <bjvanminnen@gmail.com>
2017-04-23 23:32:35 -0700

    use immutable

--- src/redux/grid.js
+++ src/redux/grid.js
@@ -1,10 +1,11 @@
+import Immutable from 'immutable';
+
 let reducers = {};
 
 const initialState = {
   rows: 0,
   cols: 0,
-  // TODO - immutable JS?
-  heights: [],
+  heights: Immutable.Map(),
   cells: []
 };
 
@@ -27,19 +28,18 @@ export const createGrid = (rows, cols) => ({
 
 reducers[CREATE_GRID] = (state, action) => {
   const { rows, cols } = action;
-  let heights = [];
+  let heights = Immutable.Map();
   let cells = [];
   for (let x = 0; x < cols + 1; x++) {
-    heights[x] = [];
     for (let y = 0; y < rows + 1; y++) {
-      heights[x][y] = 0;
+      heights = heights.set(pointKey(x, y), 0);
       if (x < cols && y < rows) {
         cells.push({x, y});
       }
     }
   }
 
-  heights[3][3] = 1;
+  heights = heights.set(pointKey(3, 3), 1);  
 
   return {
     ...state,
@@ -66,8 +66,12 @@ const TILE_HEIGHT_HALF = 32;
 const TILE_WIDTH_HALF = TILE_HEIGHT_HALF * 2;
 const HEIGHT_DELTA = 20;
 
+function pointKey(x, y) {
+  return `${x}_${y}`;
+}
+
 const pointScreenPos = (state, x, y) => {
-  const height = state.heights[x][y];
+  const height = state.heights.get(pointKey(x, y));
   return {
     x: (x - y) * TILE_WIDTH_HALF,
     y: (x + y) * TILE_HEIGHT_HALF - HEIGHT_DELTA * height,
@@ -82,8 +86,8 @@ export const getCorners = (state, x, y) => ([
 ]);
 
 export const getCornerHeights = (state, x, y) => ([
-  state.heights[x][y],
-  state.heights[x + 1][y],
-  state.heights[x + 1][y + 1],
-  state.heights[x][y + 1],
+  state.heights.get(pointKey(x, y)),
+  state.heights.get(pointKey(x + 1, y)),
+  state.heights.get(pointKey(x + 1, y + 1)),
+  state.heights.get(pointKey(x, y + 1)),
 ]);
